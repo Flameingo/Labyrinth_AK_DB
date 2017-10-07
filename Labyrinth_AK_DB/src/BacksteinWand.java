0887a2c68f30c0d48954928f714e7e80185cc960
Alex <Alex@192.168.0.73>
2017-10-07 10:55:03 +0200

    Kommentar zu BacksteinWand hinzugefügt

--- Labyrinth_AK_DB/src/BacksteinWand.java
+++ Labyrinth_AK_DB/src/BacksteinWand.java
@@ -1,7 +1,9 @@
 
 public class BacksteinWand extends Wand
 {
-
+//Dies wird die karge BacksteinWand, es gibt einen Konstruktor ohne Biegung für eine gerade Wand
+// und einen Konstruktor mit Biegung für eine gebogene Wand. Ferner enthält jeder Konstruktor Laenge sowie
+// Koordinaten und Rotationswerte.
   @Override
   void step()
   {
