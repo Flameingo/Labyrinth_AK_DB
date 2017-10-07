3fdf2e5b16847bae10ea79c187412dbf700f50a8
Alex <Alex@192.168.0.73>
2017-10-07 13:30:28 +0200

    Weitere Arbeiten an der Step und Draw-Funktion der BacksteinWand und dess

--- Labyrinth_AK_DB/src/BacksteinWand.java
+++ Labyrinth_AK_DB/src/BacksteinWand.java
@@ -1,14 +1,39 @@
+import static org.lwjgl.opengl.GL11.*;
+
 
 public class BacksteinWand extends Wand
 {
 //Dies wird die karge BacksteinWand, es gibt einen Konstruktor ohne Biegung für eine gerade Wand
-// und einen Konstruktor mit Biegung für eine gebogene Wand. 
-//  Ferner enthält jeder Konstruktor Laenge sowie
-// Koordinaten und Rotationswerte.
+//und einen Konstruktor mit Biegung für eine gebogene Wand. 
+// Ferner enthält jeder Konstruktor Laenge sowie
+//Koordinaten und Rotationswerte.
+ 
+  
+  BacksteinWand(float x, float y, float z, float alpha, float beta, float gamma, float laenge)
+  {
+    this.x=x;
+    this.y=y;
+    this.z=z;
+    this.alpha=alpha;
+    this.beta=beta;
+    this.gamma=gamma;
+    this.laenge=laenge;
+    
+  }
+  
+  
   @Override
   void step()
   {
-    // TODO Auto-generated method stub
+    glPushMatrix();
+    
+      glTranslatef(this.x,this.y,this.z);
+      glRotatef(1,alpha, beta, gamma);
+      
+      
+    glPopMatrix();
+    
+   
     
   }
 
