2fc178ebf1f8bbaeaa41978977dc4f82eed09845
Alex <Alex@192.168.0.73>
2017-10-07 10:52:18 +0200

    BacksteinWand als Unterklasse von Wand hinzugefügt, nähere Bearbeitung steht an

---
+++ Labyrinth_AK_DB/src/BacksteinWand.java
@@ -0,0 +1,26 @@
+
+public class BacksteinWand extends Wand
+{
+
+  @Override
+  void step()
+  {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  void draw()
+  {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  void drawGUI()
+  {
+    // TODO Auto-generated method stub
+    
+  }
+  
+}
