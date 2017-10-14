9f20df7dafb00d46c43242e9b2ae1e1e77774738
Alex <Alex@192.168.0.73>
2017-10-14 14:35:11 +0200

    So, WandBlock lässt sich sehen

--- Labyrinth_AK_DB/src/models3D/WandBlock.java
+++ Labyrinth_AK_DB/src/models3D/WandBlock.java
@@ -10,22 +10,36 @@ import basics.Point;
 public class WandBlock extends Objekt
 {
   
+  private int wdlaenge = 11;
+  private int wdhoehe = 25;
+  
   private Shape myShape = new Shape();
   
   public WandBlock()
   {
-    for (int k = 0; k<25; k++)
+    for (int k = 0; k<wdhoehe; k++)
     {
-      
+      if (k%2 == 0)
+      {
         
-        for (int i = 0; i<11; i++)
-        {
+      for (int i = 0; i<wdlaenge; i++)
+      {
          
-            myShape.addParam(new Quader("Mitte", 0.25f, 0.1f, 0.05f), new Point(i*0.26f,0,k*0.06f));
-           
-        }
+            myShape.addParam(new Quader("Ecke", 0.25f, 0.1f, 0.05f), new Point(i*0.26f,0,k*0.06f));
+            
+         }
+      }
+         else
+         {
+           for (int i = 0; i<wdlaenge-1;i++)
+           {
+             myShape.addParam(new Quader("Ecke", 0.125f,0.1f, 0.05f), new Point(0,0,k*0.06f));
+             myShape.addParam(new Quader("Ecke", 0.125f,0.1f,0.05f), new Point((wdlaenge-1)*0.26f+0.13f,0,k*0.06f));
+             myShape.addParam(new Quader("Ecke", 0.25f,0.1f,0.05f), new Point (i*0.26f+0.13f,0,k*0.06f));
+           }
       
-    }
+         }
+      }
   }
   public void draw()
   {
