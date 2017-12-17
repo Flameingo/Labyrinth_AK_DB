dc85ad08f95d11eeeccdbea90207decd1cd76c23
Daniel Bauer <daniel.bauer@stiegeler.net>
2017-12-17 17:20:34 +0100

    getMitte in Level hinzugefügt

--- Labyrinth_AK_DB/src/basics/Level.java
+++ Labyrinth_AK_DB/src/basics/Level.java
@@ -2,6 +2,7 @@ package basics;
 
 public class Level{
 public float x1,x2,y1,y2;
+public int a,b;
 	/**
 	 * Erleichtert den Labyrinthbau auf der waagrechten Ebene. Dabei wird ein quadratisches Raster auf diese Ebene gelegt. a und b sind die Indize
 	 * 
@@ -9,7 +10,7 @@ public float x1,x2,y1,y2;
 	 *  Waende angrenzender Felder schliessen die Luecke 
 	 *  So koennen Objekte leichter an den gewuenschten Ort innerhalb einer Labyrinthstruktur platziert werden, da diese zumeist 
 	 *  Die Koordinaten bilden eine Ebene mit z = 0 mit quadratischen Feldern. Das gewaehlte Feld wird durch a und b ausgewaehlt,
-	 *  a wird groeßer in Richtung der positiven x - Achse, b ... negativen y - Achse
+	 *  a wird groeï¿½er in Richtung der positiven x - Achse, b ... negativen y - Achse
 	 *  
 	 *  Der Mittelpunkt berechnet sich aus x = 1.5 * a - 0.75 und y = - 1.5f * b + 0.75 
 	 *  
@@ -18,6 +19,8 @@ public float x1,x2,y1,y2;
 	 */
 public Level (int a, int b) 
 {
+  this.a = a;
+  this.b = b;
 	this.x1 = a*1.5f - 1.5f; //Diese 4 Koordinaten spannen ein Quadrat auf
 	this.y1 = b*(-1.5f);
 	this.x2 = a*1.5f;
@@ -39,6 +42,11 @@ public Level (int a, int b)
 //                             .....
 //                           neg. y - Achse
 	
+}
+
+public Point getMitte()
+{
+  return new Point(1.5f * a - 0.75f,-1.5f * b + 0.75f);
 }
 
 }
